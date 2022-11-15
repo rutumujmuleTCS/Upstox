56660165d8145dee9efe84c3acc3872fa17c9c70
rutumujmuleTCS <118238590+rutumujmuleTCS@users.noreply.github.com>
2022-11-15 10:08:03 +0530

    Update Space_in_middle.java

--- src/Star_pattern/Space_in_middle.java
+++ src/Star_pattern/Space_in_middle.java
@@ -3,6 +3,7 @@ package Star_pattern;
 public class Space_in_middle {
 
 	public static void main(String[] args) {
+		System.out.println("rutuja");
 		for(int i=1;i<=3;i++) {
 			for(int j=1;j<=4;j++) {
 				if(i==1||i==3||j==1||j==4) {
